155fcc60022551ca2855710c5def6555297a712b
Rahul <rahulsharma15j@gmail.com>
2022-09-05 21:52:59 +0530

    footer started

---
+++ script.js
@@ -0,0 +1,18 @@
+const carousal = document.querySelector(".section_6_reviews_carousel");
+const leftBtn = document.querySelector(".section_6_reviews_list_controls_left");
+const rightBtn = document.querySelector(
+  ".section_6_reviews_list_controls_right"
+);
+const list = document.querySelector(".section_6_reviews_list");
+
+const onClickRight = () => {
+  carousal.scrollTo(list.scrollWidth, 0);
+  rightBtn.style.opacity = 0;
+  leftBtn.style.opacity = 1;
+};
+
+const onClickLeft = () => {
+  carousal.scrollTo(-list.scrollWidth, 0);
+  rightBtn.style.opacity = 1;
+  leftBtn.style.opacity = 0;
+};
